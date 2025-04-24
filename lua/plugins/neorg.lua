bb60b47d9cf6662d392a40aba96b60c3e7a53107
Bas Grolleman <bgrolleman@emendo-it.nl>
2025-04-24 08:50:13 +0200

    Commit cmp work

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -2,5 +2,35 @@ return {
     "nvim-neorg/neorg",
     lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
     version = "*", -- Pin Neorg to the latest stable release
-    config = true,
+    config = function()
+			require("neorg").setup({
+		    load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.ui.calendar"] = {},
+          ["core.completion"] = { config = { engine = "nvim-cmp", name = "[Norg]" } },
+          ["core.integrations.nvim-cmp"] = {},
+          ["core.esupports.metagen"] = { config = { type = "auto", update_date = true } },
+          ["core.qol.toc"] = {},
+          ["core.qol.todo_items"] = {},
+          ["core.looking-glass"] = {},
+          ["core.presenter"] = { config = { zen_mode = "zen-mode" } },
+          ["core.export"] = {},
+          ["core.export.markdown"] = { config = { extensions = "all" } },
+          ["core.summary"] = {},
+          ["core.tangle"] = { config = { report_on_empty = false } },
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/Notes.neorg",
+              },
+              default_workspace = "notes",
+            },
+          },
+        },
+      })
+    end,
 }
+
+
+
