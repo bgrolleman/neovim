23f90191447a56748c948e757722237c6fcc2061
Bas Grolleman <bgrolleman@emendo-it.nl>
2025-04-26 10:17:43 +0200

    Switch to blink and allow neorg to use that

--- init.lua
+++ init.lua
@@ -1,69 +1,6 @@
 require("config.lazy")
 require("lualine").setup()
 
--- nvim-cmp setup
---local cmp = require 'cmp'
---local luasnip = require 'luasnip'
-
---luasnip.config.setup {}
-
--- cmp.setup({
---   snippet = {
---     expand = function(args)
---       luasnip.lsp_expand(args.body)
---     end,
---   },
---   mapping = cmp.mapping.preset.insert({
---     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
---     ["<C-f>"] = cmp.mapping.scroll_docs(4),
---     ["<C-Space>"] = cmp.mapping.complete({}),
---     ["<CR>"] = cmp.mapping.confirm({
---       behavior = cmp.ConfirmBehavior.Replace,
---       select = true,
---     }),
---     ["<Tab>"] = cmp.mapping(function(fallback)
---       if cmp.visible() then
---         cmp.select_next_item()
---       elseif luasnip.expand_or_jumpable() then
---         luasnip.expand_or_jump()
---       else
---         fallback()
---       end
---     end, { "i", "s" }),
---     ["<S-Tab>"] = cmp.mapping(function(fallback)
---       if cmp.visible() then
---         cmp.select_prev_item()
---       elseif luasnip.jumpable(-1) then
---         luasnip.jump(-1)
---       else
---         fallback()
---       end
---     end, { "i", "s" }),
---   }),
---   sources = {
---     { name = "path" },
---     { name = "nvim_lsp" },
---     { name = "luasnip", keyword_length = 2 },
---     { name = "buffer", keyword_length = 3 },
---   },
---   window = {
---     documentation = cmp.config.window.bordered(),
---   },
---   formatting = {
---     fields = { "menu", "abbr", "kind" },
---     format = function(entry, item)
---       local menu_icon = {
---         nvim_lsp = "[LSP]",
---         luasnip = "[SNIP]",
---         buffer = "[BUF]",
---         path = "[PATH]",
---       }
---       item.menu = menu_icon[entry.source.name]
---       return item
---     end,
---   },
--- })
-
 local o = vim.opt
 o.compatible = false
 o.number = true
--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,36 +1,60 @@
 return {
-    "nvim-neorg/neorg",
-    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-    version = "*", -- Pin Neorg to the latest stable release
-    config = function()
-			require("neorg").setup({
-		    load = {
-          ["core.defaults"] = {}, -- Loads default behaviour
-          ["core.concealer"] = {}, -- Adds pretty icons to your documents
-          ["core.ui.calendar"] = {},
-          ["core.completion"] = { config = { engine = "nvim-cmp", name = "[Norg]" } },
-          ["core.integrations.nvim-cmp"] = {},
-          ["core.esupports.metagen"] = { config = { type = "auto", update_date = true } },
-          ["core.qol.toc"] = {},
-          ["core.qol.todo_items"] = {},
-          ["core.looking-glass"] = {},
-          ["core.presenter"] = { config = { zen_mode = "zen-mode" } },
-          ["core.export"] = {},
-          ["core.export.markdown"] = { config = { extensions = "all" } },
-          ["core.summary"] = {},
-          ["core.tangle"] = { config = { report_on_empty = false } },
-          ["core.dirman"] = { -- Manages Neorg workspaces
-            config = {
-              workspaces = {
-                notes = "~/Notes.neorg",
+  "nvim-neorg/neorg",
+  lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+  version = "*", -- Pin Neorg to the latest stable release
+  config = function()
+    require("neorg").setup({
+      load = {
+        ["core.defaults"] = {}, -- Loads default behaviour
+        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.ui.calendar"] = {},
+        ["core.completion"] = { config = { engine = { module_name = "external.lsp-completion" }, name = "[Norg]" } },
+        ["core.esupports.metagen"] = { config = { type = "auto", update_date = true } },
+        ["core.qol.toc"] = {},
+        ["core.qol.todo_items"] = {},
+        ["core.looking-glass"] = {},
+        ["core.presenter"] = { config = { zen_mode = "zen-mode" } },
+        ["core.export"] = {},
+        ["core.export.markdown"] = { config = { extensions = "all" } },
+        ["core.summary"] = {},
+        ["core.tangle"] = { config = { report_on_empty = false } },
+        ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = "~/Notes.neorg",
+            },
+            default_workspace = "notes",
+          },
+        },
+        ["external.interim-ls"] = {
+          config = {
+            -- default config shown
+            completion_provider = {
+              -- Enable or disable the completion provider
+              enable = true,
+
+              -- Show file contents as documentation when you complete a file name
+              documentation = true,
+
+              -- Try to complete categories provided by Neorg Query. Requires `benlubas/neorg-query`
+              categories = false,
+
+              -- suggest heading completions from the given file for `{@x|}` where `|` is your cursor
+              -- and `x` is an alphanumeric character. `{@name}` expands to `[name]{:$/people:# name}`
+              people = {
+                enable = false,
+
+                -- path to the file you're like to use with the `{@x` syntax, relative to the
+                -- workspace root, without the `.norg` at the end.
+                -- ie. `folder/people` results in searching `$/folder/people.norg` for headings.
+                -- Note that this will change with your workspace, so it fails silently if the file
+                -- doesn't exist
+                path = "people",
               },
-              default_workspace = "notes",
             },
           },
         },
-      })
-    end,
+      },
+    })
+  end,
 }
-
-
-
